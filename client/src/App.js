f555e9f5d74e4d311b2f4d339cf59559a93ebe2a
Gehad Salem <gehad.20334@stemalex.moe.edu.eg>
2020-11-21 18:03:42 +0200

    backend:chat

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import react from 'react';
 import './app.css';
 import {BrowserRouter} from 'react-router-dom'
 import Main from './components/MainComponent'
+import Chat from "/components/Chat/Chat"
 
 const App = () => {
   return (
--- server.js
+++ server.js
@@ -1,20 +1,33 @@
+const http = require('http');
+const socketio = require('socket.io');
 const express = require('express');
-const app = express();
 const cors = require('cors');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+
 const connectDb = require('./config/db');
 
 // connect mongodb
 connectDb();
 
+
 // apply middlewares
 app.use(cors());
 app.use(express.json({ extended: false }));
 
+
 // routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/register', require('./routes/api/register'));
+// app.use('/api/chat', require('./routes/api/chat'));
+
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Server is running on port', PORT);
-})
+});
+
