d3c715c234544732bfcfcc54bac3ff39b93f578f
antinoni <64007074+antinoni@users.noreply.github.com>
2020-11-21 18:50:18 +0200

    Add Footer

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import App from './App';
 
 const rootElement = document.getElementById("root")
