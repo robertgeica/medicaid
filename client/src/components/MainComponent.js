d0e0793ed2dbe21ea3c2150602ff0ff7941e5cfb
Keenan Akit <keenanakit@gmail.com>
2020-11-21 16:12:45 -0500

    messed up merge

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import React from 'react';
 import Login from './LoginComponent';
 import Signup from './SignupComponent';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-=======
-import React from 'react'
-import Login from './LoginComponent'
-import Signup from './SignupComponent'
-import Home from './Home'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
->>>>>>> ac4a796a7c9c7c6a6d1531c2f986f5f79cfccc85
 
 export default function Main() {
 	return (
@@ -21,7 +13,6 @@ export default function Main() {
 }
 
 const Routes = () => {
-<<<<<<< HEAD
 	return (
 		<Switch>
 			{/* new component pages to be added here */}
@@ -31,14 +22,3 @@ const Routes = () => {
 		</Switch>
 	);
 };
-=======
-    return (
-        <Switch>
-            {/* new component pages to be added here */}
-            <Route path='/login' component={Login} />
-            <Route path='/signup' component={Signup} />
-            <Route  exact path="/" component={Home} />
-        </Switch>
-    )
-}
->>>>>>> ac4a796a7c9c7c6a6d1531c2f986f5f79cfccc85
