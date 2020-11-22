36a9e7c194bc16472a1934cdbe960800cc1b3f94
Keenan Akit <keenanakit@gmail.com>
2020-11-21 19:46:12 -0500

    .

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
           <div className="footer-copyright">&copy; 2020 Medicaid. <b>
             <a href="https://www.termsofusegenerator.net/live.php?token=2LDcNfhXap6yQahIq2wU1uYPc4q8iOZl">
                &nbsp;Terms of Use 
-            </a>| 
+            </a> |  
             <a href="https://www.gdprprivacypolicy.net/live.php?token=rjPc3isBIEtpW1VgbpaK5abcPvR1KyJB">
               Privacy Policy
             </a></b>
