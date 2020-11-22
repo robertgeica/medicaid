0979077e05fd2b98964669cb5745a46c048b4bcb
Alan Pallath <iamakidilam@gmail.com>
2020-11-22 14:25:28 +1030

    double chat issue fix

--- client/src/components/ChatWidget.js
+++ client/src/components/ChatWidget.js
@@ -4,22 +4,23 @@ import io from "socket.io-client";
 import 'react-chat-widget/lib/styles.css';
 
 const socket = io('localhost:8081');
-
+var lastMessageId = 0
 export default function ChatWidget() {
     const[socketPayload, setSocketPayload] = useState({})
-    const [lastMessageId, setLastMessageId] = useState(0)
+
+    // const [lastMessageId, setLastMessageId] = useState(0)
     const [chat, setChat] = useState(null)
     socketPayload['socketID'] = socket.id
     socketPayload['_id'] = '5fb98c466f918558e85bb0d2'
 
     useEffect(() => {
         socket.on('message', ({ name, message, messageId }) => {
-            // console.log(lastMessageId, messageId)
+            console.log(lastMessageId, messageId)
             if(lastMessageId !== messageId) {
                 // console.log(lastMessageId, messageId, 'inside')
                 setChat({ name, message, messageId })
                 addResponseMessage(message)
-                setLastMessageId(messageId)
+                // setLastMessageId(messageId)
             }
           })
       },[])
@@ -28,7 +29,7 @@ export default function ChatWidget() {
     const handleNewUserMessage = (newMessage, msgCount) => {
         if (newMessage !== '') {
             const msgID = Math.floor(Math.random() * 10000);
-
+            lastMessageId = msgID
             socket.emit('message', {
                 name: 'Test',
                 message: newMessage,
